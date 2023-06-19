2703a9268f98dc75c7f2a1cadd6d76471dd95838
jtsapos <jtsapos@gmx.com>
2023-06-19 12:51:42 -0400

    Updated js\script.js

--- js/script.js
+++ js/script.js
@@ -2,22 +2,30 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
-
-
-
 /*
 For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-
-
+const studentList = document.querySelector('.student-list');
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage (list, page) {
+    //two variables to store start index and end index of list items per page given
+        
+    const startIndex = 
+    const endIndex =
 
+    studentList.innerHTML = "";
+    //loop through objects in list param
+    for (i = 0; i < list.length; i++) {
+   
+    }
+
+}
 
 
 /*
